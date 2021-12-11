76d4662b144c63dac477713a5f87502eb34ec81b
jkner <ner.jkt@gmail.com>
2021-12-11 16:32:00 -0600

    fixed updateRankings for draws

--- TestCase/index.js
+++ TestCase/index.js
@@ -19,6 +19,11 @@ app.post("/", function(req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2); 
 
+  //req.body.hasOwnProperty(win);
+  //req.body.hasOwnProperty(draw);
+  //req.body.hasOwnProperty(lose);
+
+  console.log(req.body);
   fs.readFile('./rankings/rankings.json', 'utf8', (err, array) => {
 
     if (err) {
@@ -29,20 +34,54 @@ app.post("/", function(req, res) {
         const databases = JSON.parse(array);     
 
         //{"array": [1,2,3,4,5,6]}    
-
         // print all databases
+        
         console.log("array", databases.array);
  
         var indexOfnum1 = databases.array.indexOf(num1);      
-        var indexOfnum2 = databases.array.indexOf(num2); 
+        var indexOfnum2 = databases.array.indexOf(num2);     
     
        console.log("index of num 1",indexOfnum1);
     
         //updates ranking based on the winner
-        let updatedRanking = updateRanking(databases.array, indexOfnum1, indexOfnum2);
-        writeToFile(updatedRanking);
-    
-        res.send("Current Leaderboard" + updatedRanking);          
+
+        let higherNum = checkHigherIndexVal(indexOfnum1, indexOfnum2);
+        let lowerNum = checkLowerIndexVal(indexOfnum1, indexOfnum2);
+
+        console.log("lowerNum", lowerNum, "higher num ", higherNum);
+
+        if((higherNum == indexOfnum1) && (req.body.hasOwnProperty("win") || req.body.hasOwnProperty("draw"))){ 
+            //This assumes the playerA is higher 
+            //rated than his opponent and they won, nothing changes with the ranking.
+            
+            res.send("Current Leaderboard" + databases.array);  
+        }
+        else if((higherNum == indexOfnum1) && (req.body.hasOwnProperty("loss"))){
+          
+            let updatedRanking = updateRanking(databases.array, lowerNum, higherNum);
+            writeToFile(updatedRanking);
+        
+            res.send("Current Leaderboard" + updatedRanking);   
+            
+        }
+        
+        else if((higherNum == indexOfnum2) && req.body.hasOwnProperty("win") ){
+
+            let updatedRanking = updateRanking(databases.array, lowerNum, higherNum);
+            writeToFile(updatedRanking);
+        
+            res.send("Current Leaderboard" + updatedRanking);   
+        }
+
+         
+        else if((higherNum == indexOfnum2) && (req.body.hasOwnProperty("loss") || req.body.hasOwnProperty("draw"))){      
+              
+            res.send("Current Leaderboard" + databases.array);   
+        }
+
+        else{
+            console.log("error");
+        }
     }  
 
 });
@@ -53,9 +92,7 @@ app.listen(3000, function(){
   console.log("server is running on port 3000");
 })
 
-function writeToFile(rankingArray){
-
-    let jsonStringify = JSON.stringify(rankingArray)
+function writeToFile(rankingArray){  
 
     let rankingJson = JSON.stringify({ array : rankingArray });
 
@@ -68,16 +105,36 @@ function writeToFile(rankingArray){
 }
 
 function updateRanking(data, fromIndex, toIndex){
-   // arr = Array.prototype.slice.call(arr);
-
-  // var data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
    alibarray().move(data, fromIndex, toIndex);
    
    console.log("DATA",data);
-
    return data;
+}
+
+function checkHigherIndexVal(num1, num2){
+
+    if(num1 < num2){
+        return num1
+    }
+
+    else {
+        return num2
+    }
 
 }
+
+function checkLowerIndexVal(num1, num2){
+    if(num1 > num2){
+        return num1
+    }
+    else {
+        return num2
+    }
+
+}
+
+
 
 
 
