700f93f5103abc4a88fd15cecb9c6cff2eda1bae
jkner <ner.jkt@gmail.com>
2021-12-11 22:29:19 -0600

    moved helper functions for easier testing

---
+++ TestCase/helperFunctions/functions.js
@@ -0,0 +1,35 @@
+const alibarray = require("alib-array");
+
+exports.writeToFile = function(rankingArray) {
+    let rankingJson = JSON.stringify({ array: rankingArray });
+  
+    fs.writeFile("./rankings/rankings.json", rankingJson, function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    });
+  
+    return;
+  }
+  
+exports.updateRanking = function(data, fromIndex, toIndex) {
+    alibarray().move(data, fromIndex, toIndex);
+  
+    console.log("DATA", data);
+    return data;
+  }
+  
+exports.checkHigherIndexVal = function (num1, num2) {
+    if (num1 < num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
+  
+ exports.checkLowerIndexVal= function(num1, num2) {
+    if (num1 > num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
