510a215e0f4f20055235072d728629490f54ddba
Ian Chen <ichen2@lsu.edu>
2022-01-16 19:00:19 -0600

    Created login screen

--- TestCase/react-app/src/screens/LoginScreen.js
+++ TestCase/react-app/src/screens/LoginScreen.js
@@ -1,10 +1,34 @@
 import React from "react";
-import '../App.css';
+import "./LoginScreen.css"
 
 export default function LoginScreen() {
+
     return (
-        <div style={{display: 'flex', height: '100vh'}}>
-            <h1>Login Screen</h1>
+        <div id={"container"}>
+            <img id={"banner-image"} src={require("../images/background.jpeg")} />
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', padding: 48}}>
+                <div style={{flex: 1, maxHeight: 48 }} />
+                <h1 style={{fontWeight: 600, fontSize: 20}}>Login</h1>
+                <div style={{height: 32}} />
+                <label className={"text-field-label-style"}>
+                    Email
+                    <br />
+                    <div style={{height: 8}} />
+                    <input type={"email"} className={"text-field-style"} />
+                </label>
+                <br />
+                <div style={{height: 8}} />
+                <label className={"text-field-label-style"}>
+                    Password
+                    <br />
+                    <div style={{height: 8}} />
+                    <input type={"password"} className={"text-field-style"} />
+                </label>
+                <div style={{height: 32}} />
+                <button>
+                    Login
+                </button>
+            </div>
         </div>
     );
 }
