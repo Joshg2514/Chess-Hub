76359d977fc460c66460790f02c8cb677241bd6e
Ian Chen <ichen2@lsu.edu>
2022-01-30 12:41:20 -0600

    Added a placeholder button to open the full leaderboard

--- TestCase/react-app/src/components/LeaderboardWidget.tsx
+++ TestCase/react-app/src/components/LeaderboardWidget.tsx
@@ -7,9 +7,13 @@ export default function LeaderboardWidget(props: { leaderboard: UserObj[], user?
 
     return (
         <>
-            <h4 id={"widget-header"}>
-                Leaderboard
-            </h4>
+            <div id={"widget-header"} style={{display: 'flex', alignItems: 'center'}}>
+                <h4>
+                    Leaderboard
+                </h4>
+                <div style={{flex: 1}} />
+                <span className={"widget-header-link"}>{"More \u203A\u203A"}</span>
+            </div>
             {leaderboard.map((player, index) => (
                 <div className={equals(user, player) ? "widget-item-highlight" : "widget-item"} key={index}>
                     <div style={{flex: 1}}><span className={"home-leaderboard-number"}>{index + 1}</span></div>
