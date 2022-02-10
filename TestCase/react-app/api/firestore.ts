fde40ccdad9c6ce28cd6b6d644e69cb41be6459e
Ian Chen <ichen2@lsu.edu>
2022-02-09 23:20:00 -0600

    Added firestore write in 'addUser()'

--- TestCase/react-app/api/firestore.ts
+++ TestCase/react-app/api/firestore.ts
@@ -1,5 +1,14 @@
 import { dummyUser, UserObj } from "./models/UserObj";
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const serviceAccount = require('./serviceAccountKey.json');
+require('dotenv').config({ path: __dirname + '/./../.env' });
+
+initializeApp({
+  credential: cert(serviceAccount)
+})
+const db = getFirestore()
 
 export const addUser = async (user: UserObj) => {
-  // TODO: update firestore
+  const res = await db.collection('users').doc(user.id).set(user);
 }
